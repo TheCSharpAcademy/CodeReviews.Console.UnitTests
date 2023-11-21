54b042b373b6019f3108915778c9afc35e3581ce
Marius Gravningsmyhr <marius.gravningsmyhr@gmail.com>
2023-11-21 18:23:01 +0100

    Add more test cases

--- UnitTests.mariusgrHiof/CodingTracker.Tests/ValidationIsValidDate.cs
+++ UnitTests.mariusgrHiof/CodingTracker.Tests/ValidationIsValidDate.cs
@@ -6,35 +6,50 @@ namespace CodingTracker.Tests;
 public class ValidationIsValidDate
 {
     [TestMethod]
-    public void IsValidDate_DateIsValid_ReturnTrue()
+    [DataRow("01/06/2023 14:43")]
+    [DataRow("12/06/2019 09:43")]
+    [DataRow("25/11/2003 14:43")]
+    public void IsValidDate_DateIsValid_ReturnTrue(string date)
     {
-        bool result = Validation.ValidateDate("01/06/2023 14:43");
+        bool result = Validation.ValidateDate(date);
 
-        Assert.IsTrue(result, "01/06/2023 14:43 is valid date");
+        Assert.IsTrue(result, $"{date} is valid date");
     }
 
     [TestMethod]
-    public void IsValidDate_DateIsNotValid_ReturnFalse()
+    [DataRow("01/06/2023 14:4")]
+    [DataRow(" ")]
+    [DataRow("@")]
+    [DataRow("test")]
+    public void IsValidDate_DateIsNotValid_ReturnFalse(string date)
     {
-        bool result = Validation.ValidateDate("01/06/2023 14:4");
+        bool result = Validation.ValidateDate(date);
 
-        Assert.IsFalse(result, "01/06/2023 14:4 is not a valid date");
+        Assert.IsFalse(result, $"{date} is not a valid date");
     }
 
     [TestMethod]
-    public void IsValidDateRange_DateRangeIsValid_ReturnTrue()
+    [DataRow("01/06/2023 13:00", "01/06/2023 14:00")]
+    [DataRow("05/12/2021 07:40", "05/12/2021 09:00")]
+    [DataRow("05/12/2021 07:40", "06/12/2021 03:00")]
+    public void IsValidDateRange_DateRangeIsValid_ReturnTrue(string startDate, string endDate)
     {
-        bool result = Validation.ValidateDateRange(DateTime.Parse("01/06/2023 13:00"), DateTime.Parse("01/06/2023 14:00"));
+        bool result = Validation.ValidateDateRange(startDate, endDate);
 
-        Assert.IsTrue(result, "01/06/2023 13:00 - 01/06/2023 14:00 is a valid date range");
+        Assert.IsTrue(result, $"{startDate} - {endDate} is a valid date range");
     }
 
     [TestMethod]
-    public void IsValidDateRange_DateRangeIsNotValid_ReturnFalse()
+    [DataRow("01/06/2023 13:00", "25/01/2022 14:00")]
+    [DataRow("05/12/2021 07:40", "05/12/2021 06:00")]
+    [DataRow("12/13/2021 07:40", "12/13/2021 09:00")]
+    [DataRow("12/1320 0:40", "12/1/201 09:0")]
+    [DataRow(" ", "")]
+    public void IsValidDateRange_DateRangeIsNotValid_ReturnFalse(string startDate, string endDate)
     {
-        bool result = Validation.ValidateDateRange(DateTime.Parse("01/06/2023 13:00"), DateTime.Parse("01/06/2023 12:00"));
+        bool result = Validation.ValidateDateRange(startDate, endDate);
 
-        Assert.IsFalse(result, "01/06/2023 13:00 - 01/06/2023 12:00 is NOT a valid date range");
+        Assert.IsFalse(result, $"{startDate} - {endDate} is NOT a valid date range");
     }
 
 
--- UnitTests.mariusgrHiof/CodingTracker.Tests/ValidationIsValidId.cs
+++ UnitTests.mariusgrHiof/CodingTracker.Tests/ValidationIsValidId.cs
@@ -5,20 +5,33 @@ namespace CodingTracker.Tests;
 [TestClass]
 public class ValidationIsValidId
 {
-    [TestMethod]
-    public void IsValidId_Id1IsValid_ReturnTrue()
-    {
-        bool result = Validation.ValidateId("1");
 
-        Assert.IsTrue(result, "1 is valid Id");
+    [TestMethod]
+    [DataRow("1")]
+    [DataRow("2")]
+    [DataRow("3")]
+    [DataRow("123")]
+    [DataRow("243")]
+    public void IsValidId_IdIsValid_ReturnTrue(string value)
+    {
+        bool result = Validation.ValidateId(value);
+
+        Assert.IsTrue(result, $"{value} is valid Id");
     }
 
     [TestMethod]
-    public void IsValidId_IdaIsNotValid_ReturnFalse()
+    [DataRow("a")]
+    [DataRow("-1")]
+    [DataRow("0")]
+    [DataRow("@")]
+    [DataRow("%")]
+    [DataRow("")]
+    [DataRow(" ")]
+    public void IsValidId_IdIsNotValid_ReturnFalse(string value)
     {
-        bool result = Validation.ValidateId("a");
+        bool result = Validation.ValidateId(value);
 
-        Assert.IsFalse(result, "a is NOT a valid Id");
+        Assert.IsFalse(result, $"{value} is NOT a valid Id");
     }
 
 
