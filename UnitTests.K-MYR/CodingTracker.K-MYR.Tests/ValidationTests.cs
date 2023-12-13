aefcd02722095eb8aa367f2f17a4ff315e0432f4
K-MYR <144546255+K-MYR@users.noreply.github.com>
2023-12-13 10:45:13 +0100

    Project Setup

---
+++ UnitTests.K-MYR/CodingTracker.K-MYR.Tests/ValidationTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingTracker.K_MYR;
+
+namespace CodingTracker.K_MYR.Tests;
+
+[TestClass]
+public class ValidationTests
+{
+    [TestMethod]
+    public void ValidateDate_ValidInputs_ReturnTrue()
+    {
+    }
+}
---
+++ UnitTests.K-MYR/CodingTracker.K-MYR/Models/CodingGoal.cs
@@ -0,0 +1,48 @@
+﻿namespace CodingTracker.K_MYR.Models;
+
+internal class CodingGoal
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime Deadline { get; set; }
+
+    public TimeSpan Goal { get; set; }
+
+    public TimeSpan ElapsedTime { get; set; }
+
+    public string ElapsedPercentage
+    {
+        get
+        {
+            if (ElapsedTime < Goal)
+            {
+                double percentage = Math.Round(ElapsedTime / Goal, 4);
+                return string.Format("{0:P2}", percentage);
+            }
+            else
+                return "100%";
+        }
+    }
+
+    public TimeSpan AverageHoursToGoal
+    {
+        get
+        {
+            double days = (Deadline - DateTime.Now).TotalDays;
+
+            if (days > 0 && Goal > ElapsedTime)
+            {
+                TimeSpan averageHours = ((Goal - ElapsedTime) / days);
+                return TimeSpan.FromSeconds(Math.Round(averageHours.TotalSeconds));
+            }
+            else
+            {
+                return new TimeSpan(0, 0, 0);
+            }
+        }
+    }
+}
