89e207f93b72eccc24cd362f7a7539882f821fd2
Michiel <michiel.vergauwen@visma.com>
2023-12-21 15:20:42 +0100

    Initial commit

---
+++ UnitTests.MicVerg/CodingTracker.UnitTests/ValidationTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CodingTracker;
+
+namespace CodingTracker.UnitTests
+{
+    [TestClass]
+    public class ValidationTests
+    {
+        [TestMethod]
+        public void CanBeParsedAndValidated_CorrectDateFormat_ReturnsParsedDate()
+        {
+            
+        }
+    }
+}
