96d8603f6c21ca439bc1ad54392b10dbeea08b3c
Dejmenek <dejmen14567@gmail.com>
2024-04-08 23:08:09 +0200

    Add coding tracker project

---
+++ UnitTests.Dejmenek/CodingTracker.Dejmenek/Enums/MenuOptions.cs
@@ -0,0 +1,18 @@
+﻿namespace CodingTracker.Dejmenek.Enums
+{
+    public enum MenuOptions
+    {
+        NewCodingSession,
+        AddCodingSession,
+        DeleteCodingSession,
+        ListCodingSessions,
+        GenerateMonthlyCodingSessionsReport,
+        GenerateYearlyCodingSessionsReport,
+        SetGoal,
+        DeleteGoal,
+        UpdateGoal,
+        ListGoals,
+        ShowGoalProgress,
+        Exit
+    }
+}
