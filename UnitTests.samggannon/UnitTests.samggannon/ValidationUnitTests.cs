982cbdb3be77ccbd12fd5920597560a819dc3909
Samuel <Sam.Gannon@sportssouth.biz>
2024-07-17 14:23:29 -0500

    // final commit

--- UnitTests.samggannon/UnitTests.samggannon/ValidationUnitTests.cs
+++ UnitTests.samggannon/UnitTests.samggannon/ValidationUnitTests.cs
@@ -34,4 +34,47 @@ public class ValidationUnitTests
         // Assert
         Assert.AreEqual(result, expectedResult);
     }
+
+    [TestMethod]
+    [DataRow("07-17-24", true)]
+    public void TestIsValidDate_ValidDate_ReturnsTrue(string date, bool expectedResult)
+    {
+        var validation = new Validation();
+        bool result = validation.IsValidDate(date);
+        Assert.AreEqual(result, expectedResult);
+    }
+
+    [TestMethod]
+    [DataRow("July 17th, 2024", false)]
+    [DataRow("07172024", false)]
+    [DataRow("20240717", false)]
+    [DataRow("07/17/24", false)]
+    public void TestIsValidDate_InValidDate_ReturnsFalse(string date, bool expectedResult)
+    {
+        var validation = new Validation();
+        bool result = validation.IsValidDate(date);
+        Assert.AreEqual(result, expectedResult);
+    }
+
+    [TestMethod]
+    [DataRow("1", true)]
+    [DataRow("12", true)]
+    [DataRow("123", true)]
+    public void TestIsValidId_ValidId_ReturnsTrue(string id, bool expectedResult)
+    {
+        var validation = new Validation();
+        bool result = validation.IsValidId(id);
+        Assert.AreEqual(result, expectedResult);
+    }
+
+    [TestMethod]
+    [DataRow("-1", false)]
+    [DataRow("0.12", false)]
+    [DataRow("one", false)]
+    public void TestIsValidId_InvalidId_ReturnsFalse(string id, bool expectedResult)
+    {
+        var validation = new Validation();
+        bool result = validation.IsValidId(id);
+        Assert.AreEqual(result, expectedResult);
+    }
 }
