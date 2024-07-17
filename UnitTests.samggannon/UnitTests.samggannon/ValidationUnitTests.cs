205a72e85eb7b66865a95fbd1a744bef71c45486
Samuel <Sam.Gannon@sportssouth.biz>
2024-07-17 15:19:05 -0500

    // suggested codacy fixes

--- UnitTests.samggannon/UnitTests.samggannon/ValidationUnitTests.cs
+++ UnitTests.samggannon/UnitTests.samggannon/ValidationUnitTests.cs
@@ -8,22 +8,9 @@ public class ValidationUnitTests
     [TestMethod]
     [DataRow("12:00", true)]
     [DataRow("01:00", true)]
-    public void TestIsValidTimeFormat_ValidFormat_ReturnsTrue(string mockInput, bool expectedResult)
-    {
-        // Arrange
-        var validation = new Validation();
-
-        // Act
-        bool result = validation.IsValidTimeFormat(mockInput);
-
-        // Assert
-        Assert.AreEqual(result, expectedResult);
-    }
-
-    [TestMethod]
     [DataRow("1:00", false)]
     [DataRow("string input", false)]
-    public void TestIsValidTimeFormat_InvalidFormat_ReturnsFalse(string mockInput, bool expectedResult)
+    public void TestIsValidTimeFormat_ValidFormat_ReturnsTrueIfValid(string mockInput, bool expectedResult)
     {
         // Arrange
         var validation = new Validation();
@@ -37,19 +24,11 @@ public class ValidationUnitTests
 
     [TestMethod]
     [DataRow("07-17-24", true)]
-    public void TestIsValidDate_ValidDate_ReturnsTrue(string date, bool expectedResult)
-    {
-        var validation = new Validation();
-        bool result = validation.IsValidDate(date);
-        Assert.AreEqual(result, expectedResult);
-    }
-
-    [TestMethod]
     [DataRow("July 17th, 2024", false)]
     [DataRow("07172024", false)]
     [DataRow("20240717", false)]
     [DataRow("07/17/24", false)]
-    public void TestIsValidDate_InValidDate_ReturnsFalse(string date, bool expectedResult)
+    public void TestIsValidDate_ValidDate_ReturnsTrueIfValid(string date, bool expectedResult)
     {
         var validation = new Validation();
         bool result = validation.IsValidDate(date);
@@ -60,18 +39,10 @@ public class ValidationUnitTests
     [DataRow("1", true)]
     [DataRow("12", true)]
     [DataRow("123", true)]
-    public void TestIsValidId_ValidId_ReturnsTrue(string id, bool expectedResult)
-    {
-        var validation = new Validation();
-        bool result = validation.IsValidId(id);
-        Assert.AreEqual(result, expectedResult);
-    }
-
-    [TestMethod]
     [DataRow("-1", false)]
     [DataRow("0.12", false)]
     [DataRow("one", false)]
-    public void TestIsValidId_InvalidId_ReturnsFalse(string id, bool expectedResult)
+    public void TestIsValidId_ValidId_ReturnsTrueIfValid(string id, bool expectedResult)
     {
         var validation = new Validation();
         bool result = validation.IsValidId(id);
