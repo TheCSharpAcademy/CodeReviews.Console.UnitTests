c93872a04828f419c86cedc6a429e5a5f45706b0
harryL <harryleigh@hotmail.co.uk>
2025-01-26 14:40:06 +0000

    Project Finished!

---
+++ UnitTests.HarryJLeigh/CodingTracker.Tests/DateValidatorTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodingTracker.Helpers;
+
+namespace CodingTracker.Tests;
+
+[TestClass]
+public class CodingTrackerTests
+{
+    [TestMethod]
+    public void IsValidEndDate_ShouldReturnTrue_WhenEndDateIsGreaterThanStartDate()
+    {
+        DateTime endDate = new DateTime(2025,1, 12, 14, 00,00);
+        DateTime startDate = new DateTime(2025,1, 11, 14, 00,00);
+        bool isValid = DateValidator.IsValidEndDate(startDate, endDate);
+        Assert.IsTrue(isValid);
+    }
+
+    [TestMethod]
+    public void IsValidEndDate_ShouldReturnFalse_WhenEndDateIsLessThanStartDate()
+    {
+        DateTime endDate = new DateTime(2025,1, 11, 14, 00,00);
+        DateTime startDate = new DateTime(2025,1, 12, 14, 00,00);
+        bool isValid = DateValidator.IsValidEndDate(startDate, endDate);
+        Assert.IsFalse(isValid);
+    }
+}
