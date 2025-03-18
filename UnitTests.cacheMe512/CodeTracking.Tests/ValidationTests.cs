1f579152ecdc0187b1757aad741213acfa1de802
Kornel Filipiak <kornelf048@gmail.com>
2025-03-18 12:04:42 -0400

    add dateTime in sequence tests

--- UnitTests.cacheMe512/CodeTracking.Tests/ValidationTests.cs
+++ UnitTests.cacheMe512/CodeTracking.Tests/ValidationTests.cs
@@ -7,12 +7,33 @@ namespace CodeTracking.Tests;
 public class ValidationTests
 {
     [TestMethod]
-    public void DateTimeInSequence_ValidDates_ReturnsTrue()
+    public void DateTimeInSequence_ValidDates()
     {
         DateTime startTime = new DateTime(2025, 3, 1, 12, 0, 0);
         DateTime endTime = new DateTime(2025, 3, 1, 14, 0, 0);
 
         bool result = Validation.DateTimeInSequence(startTime, endTime);
+
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void DateTimeInSequence_EndBeforeStart()
+    {
+        DateTime startTime = new DateTime(2025, 3, 1, 14, 0, 0);
+        DateTime endTime = new DateTime(2025, 3, 1, 12, 0, 0);
+
+        bool result = Validation.DateTimeInSequence(startTime, endTime);
+
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public void DateTimeInSequence_SameDateTime()
+    {
+        DateTime dateTime = new DateTime(2025, 3, 1, 12, 0, 0);
+
+        bool result = Validation.DateTimeInSequence(dateTime, dateTime);
 
         Assert.IsTrue(result);
     }
