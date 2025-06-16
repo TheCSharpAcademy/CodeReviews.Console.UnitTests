7a5e5f471512d4d30125d0a7255791979db74d47
Brozda <daniel.brozik@gmail.com>
2025-06-16 23:23:20 +0200

    Feat: Implement unit tests

--- UnitTests.BrozDa/CodingTracker/CodingTracker.UnitTests/ValidatorTests.cs
+++ UnitTests.BrozDa/CodingTracker/CodingTracker.UnitTests/ValidatorTests.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Globalization;
 
+using CodingTracker;
 
 namespace CodingTracker.UnitTests;
 
@@ -13,29 +14,126 @@ public class ValidatorTests
     {
         string dateFormat = "dd-MM-yyyy HH:mm";
 
-        var result = DateTime.TryParseExact(DateTime.Now.ToString(dateFormat), dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+        bool result = Validator.IsValidDate(DateTime.Now.ToString(dateFormat), dateFormat);
 
         Assert.IsTrue(result);
     }
-    public void IsValidDate_InValidDateFormat_ReturnsFalse() { }
+    [TestMethod]
+    public void IsValidDate_InValidDateFormat_ReturnsFalse() 
+    {
+        string dateFormat = "dd-MM-yyyy HH:mm";
 
+        bool result = Validator.IsValidDate(DateTime.Now.ToString(), dateFormat);
 
-    public void IsValidEndDate_ValidDateFormatAfterStartDate_ReturnTrue() { }
+        Assert.IsFalse(result);
+    }
+    [TestMethod]
+    public void IsValidEndDate_ValidDateFormatAfterStartDate_ReturnTrue() 
+    {
+        string dateFormat = "dd-MM-yyyy HH:mm";
+        DateTime startDate = new DateTime(2025, 06, 16, 08, 30, 0);
+        string endDateString = startDate.AddDays(1).ToString(dateFormat);
 
-    public void IsValidEndDate_ValidDateFormatSameAsStartDate_ReturnFalse() { }
+        bool result = Validator.IsValidEndDate(endDateString,startDate,dateFormat);
 
-    public void IsValidEndDate_ValidDateFormatBeforeStartDate_ReturnFalse() { }
+        Assert.IsTrue(result);
+    }
+    [TestMethod]
+    public void IsValidEndDate_ValidDateFormatSameAsStartDate_ReturnFalse() 
+    {
+        string dateFormat = "dd-MM-yyyy HH:mm";
+        DateTime startDate = new DateTime(2025, 06, 16, 08, 30, 0);
+        string endDateString = startDate.ToString(dateFormat);
 
-    public void IsValidEndDate_InvalidDateFormatAfterStartDate_ReturnFalse() { }
+        bool result = Validator.IsValidEndDate(endDateString, startDate, dateFormat);
 
-    public void IsValidEndDate_InvalidDateFormatSameAsStartDate_ReturnFalse() { }
+        Assert.IsFalse(result);
+    }
+    [TestMethod]
+    public void IsValidEndDate_ValidDateFormatBeforeStartDate_ReturnFalse() 
+    {
+        string dateFormat = "dd-MM-yyyy HH:mm";
+        DateTime startDate = new DateTime(2025, 06, 16, 08, 30, 0);
+        string endDateString = startDate.AddHours(-2).ToString(dateFormat);
 
-    public void IsValidEndDate_InvalidDateFormatBeforeStartDate_ReturnFalse() { }
+        bool result = Validator.IsValidEndDate(endDateString, startDate, dateFormat);
 
-    public void IsSessionIdValidOrZero_ValidIdFromSet_ReturnsTrue() { }
-    public void IsSessionIdValidOrZero_InvalidIdFromSet_ReturnsFalse() { }
+        Assert.IsFalse(result);
+    }
+    [TestMethod]
+    public void IsValidEndDate_InvalidDateFormatAfterStartDate_ReturnFalse() 
+    {
+        string dateFormat = "dd-MM-yyyy HH:mm";
+        DateTime startDate = new DateTime(2025, 06, 16, 08, 30, 0);
+        string endDateString = startDate.AddDays(1).ToString();
 
-    public void IsSessionIdValidOrZero_ZeroInputSetEmpty_ReturnsFalse() { }
+        bool result = Validator.IsValidEndDate(endDateString, startDate, dateFormat);
 
-    public void IsSessionIdValidOrZero_ZeroInputSetFull_ReturnsTrue() { }
+        Assert.IsFalse(result);
+
+    }
+    [TestMethod]
+    public void IsValidEndDate_InvalidDateFormatSameAsStartDate_ReturnFalse() 
+    {
+        string dateFormat = "dd-MM-yyyy HH:mm";
+        DateTime startDate = new DateTime(2025, 06, 16, 08, 30, 0);
+        string endDateString = startDate.ToString();
+
+        bool result = Validator.IsValidEndDate(endDateString, startDate, dateFormat);
+
+        Assert.IsFalse(result);
+    }
+    [TestMethod]
+    public void IsValidEndDate_InvalidDateFormatBeforeStartDate_ReturnFalse() 
+    {
+        string dateFormat = "dd-MM-yyyy HH:mm";
+        DateTime startDate = new DateTime(2025, 06, 16, 08, 30, 0);
+        string endDateString = startDate.AddHours(-2).ToString(dateFormat);
+
+        bool result = Validator.IsValidEndDate(endDateString, startDate, dateFormat);
+
+        Assert.IsFalse(result);
+    }
+    [TestMethod]
+    public void IsSessionIdValidOrZero_ValidIdFromSet_ReturnsTrue() 
+    { 
+        HashSet<int> validIds = new HashSet<int>() { 1,3,8,9,15,27,93};
+        int input = 3;
+
+        bool result = Validator.IsSessionIdValidOrZero(input, validIds);
+
+        Assert.IsTrue(result);
+    }
+    [TestMethod]
+    public void IsSessionIdValidOrZero_InvalidIdFromSet_ReturnsFalse() 
+    {
+        HashSet<int> validIds = new HashSet<int>() { 1, 3, 8, 9, 15, 27, 93 };
+        int input = 5;
+
+        bool result = Validator.IsSessionIdValidOrZero(input, validIds);
+
+        Assert.IsFalse(result);
+    }
+    [TestMethod]
+    public void IsSessionIdValidOrZero_ZeroInputSetFull_ReturnsTrue() 
+    {
+        HashSet<int> validIds = new HashSet<int>() { 1, 3, 8, 9, 15, 27, 93 };
+        int input = 0;
+
+        bool result = Validator.IsSessionIdValidOrZero(input, validIds);
+
+        Assert.IsTrue(result);
+    }
+    [TestMethod]
+    public void IsSessionIdValidOrZero_ZeroInputSetEmpty_ReturnsTrue() 
+    {
+        HashSet<int> validIds = new HashSet<int>();
+        int input = 0;
+
+        bool result = Validator.IsSessionIdValidOrZero(input, validIds);
+
+        Assert.IsTrue(result);
+    }
+
+    
 }
